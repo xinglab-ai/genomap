194c3af33a6022554e4e5ff7d901f5c170c4c3e0
Vasudha Jha <vasudhaj@CWQK7RYW00.local>
2023-07-07 14:28:57 -0700

    exclude packages using setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,38 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    setup()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="test-vasudha-genomap",
+    version="0.0.12",
+    author="Vasudha Jha",
+    author_email="reachvasudha27@gmail.com",
+    description="Create images from gene expression data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xinglab-ai/genomap",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=("genomap.genoNet", "genomap.genoNetus")),
+    python_requires=">=3.7",
+    install_requires=[
+        "torch",
+        "torchvision",
+        "scipy",
+        "scikit-learn",
+        "pyclustering",
+        "pot",
+        "phate",
+        "pandas",
+        "opencv-python-headless",
+        "numpy",
+        "matplotlib",
+        "jupyterlab",
+        "jupyter",
+        "grad-cam",
+    ],
+    include_package_data=True,
+)
