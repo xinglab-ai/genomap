fad9adf2a90694e802ed887f93a4b896e296c279
Vasudha Jha <vasudhaj@CWQK7RYW00.local>
2023-07-06 21:27:00 -0700

    added extra files for init

--- genomap/__init__.py
+++ genomap/__init__.py
@@ -1,2 +1,6 @@
 # from genomap import construct_genomap
-from .genomap import construct_genomap
+from .genomap import *
+from .genomapOPT import *
+from .genoNet import *
+from .genoNetus import *
+
